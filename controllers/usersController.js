1127985e0565dd6affa2f154945787bb06f67916
assalameslmani <139454177+assalameslmani@users.noreply.github.com>
2023-10-19 10:24:03 +0300

    Attendance Backend

---
+++ controllers/usersController.js
@@ -0,0 +1,72 @@
+const db = require ('../config/db');
+
+   /*get*/
+   const getUsersQuery = async (req, res) => {
+    const { role } = req.params;
+  const { name, email } = req.query;
+
+  let getUsersQuery = `SELECT * FROM users WHERE role = ?`;
+
+  if (name) {
+    getUsersQuery += ` AND name LIKE '%${name}%'`;
+  }
+  if (email) {
+    getUsersQuery += ` AND email LIKE '%${email}%'`;
+  }
+
+  databaseConnection.query(getUsersQuery, [role], (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error fetching users');
+    } else {
+      res.status(200).json(rows);
+    }
+  });
+};
+
+/*put*/
+const deleteUserQuery = async (req, res) => {
+    const { id } = req.params;
+  const { role } = req.query;
+  const deleteUserQuery = `DELETE FROM users WHERE id = ? AND role = ?`;
+
+  databaseConnection.query(deleteUserQuery, [id, role], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error deleting user');
+    } else {
+      if (result.affectedRows === 0) {
+        res.status(404).send('User not found');
+      } else {
+        res.sendStatus(200);
+      }
+    }
+  });
+};
+
+/*delete*/
+const updateUserQuery = async (req, res) => {
+const { id } = req.params;
+  const { name, email, password } = req.body; 
+
+  const updateUserQuery = `UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?`;
+
+  databaseConnection.query(
+    updateUserQuery,
+    [name, email, password, id],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error updating user');
+      } else {
+        if (result.affectedRows === 0) {
+          res.status(404).send('User not found');
+        } else {
+          res.sendStatus(200);
+        }
+      }
+    }
+  );
+};
+
+module.exports = { getUsersQuery, updateUserQuery, deleteUserQuery };
--- server.js
+++ server.js
@@ -1,28 +1,23 @@
-const express = require('express');
-const mysql = require('mysql2');
+require('dotenv').config();
+const express = require ('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const app = express();
-app.use(cors());
-const usersRouter = require('./routes/users'); 
-const port = 5000;
+const cors = require('cors');
+const PORT = process.env.PORT;
+require('./config/db');
+const usersRouter = require('./routes/userRoute'); 
+const lessonRoutes = require('./routes/lessonRoute');
+const attendanceRoute = require('./routes/attendanceRoute');
+
 app.use(bodyParser.json());
 app.use('/users', usersRouter);
+app.use('/lessons', lessonRoutes);
+app.use('/attendance', attendanceRoute);
+app.use(cors());
 
-
-const connection = mysql.createConnection({
-  host: 'db4free.net',
-  user: 'hassanatsimplon',
-  password: '12ab34xy',
-  database: 'languagelearning',
-  port: 3306 
+app.listen(PORT, () =>{
+    console.log(`Server is running on PORT ${PORT} `);
 });
-
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to MySQL server!');
-});
-
 
 
 app.post('/login', (req, res) => {
@@ -83,9 +78,3 @@ app.post('/signup', (req, res) => {
       }
     });
   });
-
-
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
